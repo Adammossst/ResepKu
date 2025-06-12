723507be35db5173608e8101ee847e182425255e
Adammossst <adamsaja331@gmail.com>
2025-06-12 07:33:51 +0700

    Menambahkan token agar dapat menyimpan didatastorenya

--- app/src/main/java/com/adamchaniago0025/assesment3/network/UserDataStore.kt
+++ app/src/main/java/com/adamchaniago0025/assesment3/network/UserDataStore.kt
@@ -16,12 +16,14 @@ val Context.dataStore : DataStore<Preferences> by preferencesDataStore(
 
 class UserDataStore(private val context: Context) {
     companion object {
+        private val USER_TOKEN = stringPreferencesKey("token")
         private val USER_NAME = stringPreferencesKey("name")
         private val USER_EMAIL = stringPreferencesKey("email")
         private val USER_PHOTO = stringPreferencesKey("photoUrl")
     }
     val userFlow: Flow<User> = context.dataStore.data.map { preferences ->
         User(
+            token = preferences[USER_TOKEN] ?: "",
             name = preferences[USER_NAME] ?: "",
             email = preferences[USER_EMAIL] ?: "",
             photoUrl = preferences[USER_PHOTO] ?: ""
@@ -29,6 +31,7 @@ class UserDataStore(private val context: Context) {
     }
     suspend fun saveData(user: User){
         context.dataStore.edit { preferences ->
+            preferences[USER_TOKEN] = user.token
             preferences[USER_NAME] = user.name
             preferences[USER_EMAIL] = user.email
             preferences[USER_PHOTO] = user.photoUrl
